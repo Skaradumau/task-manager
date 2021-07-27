522e7784ec67a3fe9882f210d4b131f3a4514f8e
Y. Skaradumau <y.skaradumau@godeltech.com>
2021-07-27 18:52:00 +0300

    WIP

--- src/modules/Tasks/Tasks.js
+++ src/modules/Tasks/Tasks.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Button } from 'components/atoms'
+import { useDispatch } from 'react-redux'
 
-const Tasks = () => (
-  <div>
+const Tasks = () => {
+  const dispatch = useDispatch()
+
+  const onClick = () => {
+    dispatch({type: 'TASKS_REQUESTED'})
+  }
+
+  return <div>
     <h1>Processes</h1>
-    <Button>Create</Button>
+    <Button onClick={onClick}>Create</Button>
   </div>
-)
+}
 
 export default Tasks
--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -1,5 +1,17 @@
-import { all } from 'redux-saga/effects'
+import { all, takeEvery, delay } from 'redux-saga/effects'
+import { fetchTasks } from 'api/tasks'
+
+export function* watchTasksFetch() {
+  yield takeEvery('TASKS_REQUESTED', doSomething)
+}
+
+export function* doSomething() {
+  const tasks = yield fetchTasks()
+  yield put({ type: 'TASKS_RECEIVED', tasks })
+}
 
 export default function* () {
-  yield all([])
+  yield all([
+    watchTasksFetch()
+  ])
 }
